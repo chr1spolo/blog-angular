161c8511466c4c89b6911a444c7adf846c5415d8
chr1spolo <polidavis28@gmail.com>
2018-02-27 22:52:37 -0500

    show component style

--- src/app/show/show.component.ts
+++ src/app/show/show.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var UIkit: any;
+
+
 @Component({
   selector: 'app-show',
   templateUrl: './show.component.html',
   styleUrls: ['./show.component.css']
 })
 export class ShowComponent implements OnInit {
-
+  kit;
   constructor() { }
 
   ngOnInit() {
+    let element = document.getElementsByClassName('see_more');
+    UIkit.icon(element).svg.then( function(svg) {
+      svg.querySelector('polyline').style.stroke = '#040d45'; 
+    })
   }
 
 }
